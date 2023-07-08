31d1267a49511aec28c4fd654e13622fe3d2a12d
HEMANTH KUMAR REDDY TATAGARI <110995141+HemuTatagari@users.noreply.github.com>
2023-07-08 23:20:24 +0530

    Update flip_bits.java

--- flip_bits.java
+++ flip_bits.java
@@ -1,3 +1,39 @@
+/*
+
+You are given two numbers A and B. Write a program to count the number of bits to be flipped to change the number A to the number B. Flipping a bit of a number means changing a bit from 1 to 0 or vice versa.
+
+Input Format
+
+First line of input contains T - number of test cases. Each of the next T lines contains 2 integer A and B, separated by space.
+
+Constraints
+
+1 <= T <= 100000
+0 <= N <= 109
+
+Output Format
+
+For each test case, print the number of bit flips required to convert A to B, separated by new line.
+
+Sample Input 0
+
+4
+20 10
+16 8
+1 153
+549 24
+Sample Output 0
+
+4
+2
+3
+6
+Explanation 0
+
+Self Explanatory
+
+*/
+
 import java.io.*;
 import java.util.*;
 
