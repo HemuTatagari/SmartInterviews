cd89fa7623f7f3634bc4777d35010b777cef46c8
HEMANTH KUMAR REDDY TATAGARI <110995141+HemuTatagari@users.noreply.github.com>
2023-07-08 23:16:56 +0530

    Update count_set_bits.java

--- count_set_bits.java
+++ count_set_bits.java
@@ -1,3 +1,50 @@
+/*
+
+Given a number N, find the number of bits that are set to 1 in its binary representation.
+
+Input Format
+
+First line of input contains T - the number of test cases. It is followed by T lines, each line contains a single integer N.
+
+Constraints
+
+1 <= T <= 104
+0 <= N <= 1018
+
+Output Format
+
+For each test case, print the number of bits set to 1 in the binary representation of N, separated by a new line.
+
+Sample Input 0
+
+3
+4
+15
+10
+Sample Output 0
+
+1
+4
+2
+Explanation 0
+
+Test-Case 1:
+The binary representation of 4 is 100. The number of 1's in the binary representation of 4 is 1.
+
+Test-Case 2:
+The binary representation of 15 is 1111. The number of 1's in the binary representation of 15 is 4.
+
+Submissions: 6870
+Max Score: 50
+Difficulty: Easy
+Rate This Challenge:
+
+    
+More
+
+
+*/
+
 import java.io.*;
 import java.util.*;
 import java.text.*;
