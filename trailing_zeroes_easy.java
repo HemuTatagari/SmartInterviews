4e8a9fa8452f0181afb4dc9f001809bbae16a2ef
HEMANTH KUMAR REDDY TATAGARI <110995141+HemuTatagari@users.noreply.github.com>
2023-07-08 23:24:49 +0530

    Update trailing_zeroes_easy.java

--- trailing_zeroes_easy.java
+++ trailing_zeroes_easy.java
@@ -1,3 +1,39 @@
+/*
+
+Count the number of trailing 0s in factorial of a given number.
+
+Input Format
+
+First line of input contains T - number of test cases. Its followed by T lines, each containing an integer N.
+
+Constraints
+
+1 <= T <= 10000
+1 <= N <= 1015
+
+Output Format
+
+For each test case, print the number of trailing 0s in N!, separated by new line.
+
+Sample Input 0
+
+3
+2
+5
+20
+Sample Output 0
+
+0
+1
+4
+Explanation 0
+
+2! = 2 [No trailing zeros]
+5! = 120 [Trailing zeros=1]
+20! = 2432902008176640000 [Trailing zeros=4]
+
+*/
+
 import java.io.*;
 import java.util.*;
 import java.text.*;
