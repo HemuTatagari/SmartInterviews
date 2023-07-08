6c6bf4e4af2f35561101f04f18e8bcc84390b037
HEMANTH KUMAR REDDY TATAGARI <110995141+HemuTatagari@users.noreply.github.com>
2023-07-08 23:15:45 +0530

    Update check_power_of_2.java

--- check_power_of_2.java
+++ check_power_of_2.java
@@ -1,3 +1,41 @@
+/*
+
+Given a number, check if it is a power of 2.
+
+Input Format
+
+First line of input contains T - number of test cases. Its followed by T lines, each line containing a single positive integer.
+
+Constraints
+
+1 <= T <= 10000
+1 <= N <= 1018
+
+Output Format
+
+For each test case, print "True" or "False", separated by new line.
+
+Sample Input 0
+
+5
+1
+8
+10
+25
+512
+Sample Output 0
+
+True
+True
+False
+False
+True
+Explanation 0
+
+Self Explanatory
+
+*/
+
 import java.io.*;
 import java.util.*;
 import java.text.*;
