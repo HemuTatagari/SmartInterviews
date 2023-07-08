bdeda47367424aa50099a618e981ba616a355ad9
HEMANTH KUMAR REDDY TATAGARI <110995141+HemuTatagari@users.noreply.github.com>
2023-07-08 23:25:55 +0530

    Update lcm_and_hcf.java

--- lcm_and_hcf.java
+++ lcm_and_hcf.java
@@ -1,3 +1,41 @@
+/*
+
+
+Given 2 numbers, find their LCM and HCF.
+Note: Do not use any inbuilt functions/libraries for your main logic.
+
+Input Format
+
+First line of input contains T - number of test cases. Its followed by T lines, each contains 2 numbers A and B.
+
+Constraints
+
+1 <= T <= 105
+1 <= A,B <= 109
+
+Output Format
+
+For each test case, print their LCM and HCF separated by space, separated by newline.
+
+Sample Input 0
+
+4
+4 710
+13 1
+6 4
+605904 996510762
+Sample Output 0
+
+1420 2
+13 1
+12 2
+7740895599216 78
+Explanation 0
+
+Self Explanatory
+
+*/
+
 import java.io.*;
 import java.util.*;
 import java.text.*;
