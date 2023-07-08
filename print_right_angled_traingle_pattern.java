7d39235b2fcb2b0b0b3645f72dc1551602583edf
HEMANTH KUMAR REDDY TATAGARI <110995141+HemuTatagari@users.noreply.github.com>
2023-07-08 23:21:44 +0530

    Update print_right_angled_traingle_pattern.java

--- print_right_angled_traingle_pattern.java
+++ print_right_angled_traingle_pattern.java
@@ -1,3 +1,57 @@
+/*
+
+Print mirror image of right-angled triangle using '*'. See examples for more details.
+
+Input Format
+
+First line of input contains T - number of test cases. Its followed by T lines, each line contains a single integer N - the size of the pattern.
+
+Constraints
+
+1 <= T <= 100
+1 <= N <= 100
+
+Output Format
+
+For each test case, print the test case number as shown, followed by the pattern, separated by newline.
+
+Sample Input 0
+
+4
+2
+1
+5
+10
+Sample Output 0
+
+Case #1:
+ *
+**
+Case #2:
+*
+Case #3:
+    *
+   **
+  ***
+ ****
+*****
+Case #4:
+         *
+        **
+       ***
+      ****
+     *****
+    ******
+   *******
+  ********
+ *********
+**********
+Explanation 0
+
+Self Explanatory
+
+*/
+
 import java.io.*;
 import java.util.*;
 
