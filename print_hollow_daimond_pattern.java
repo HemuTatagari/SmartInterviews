800af5d860aa2972e9b1c6cfeadb334220567337
HEMANTH KUMAR REDDY TATAGARI <110995141+HemuTatagari@users.noreply.github.com>
2023-07-08 23:22:19 +0530

    Update print_hollow_daimond_pattern.java

--- print_hollow_daimond_pattern.java
+++ print_hollow_daimond_pattern.java
@@ -1,3 +1,69 @@
+/*
+
+Print hollow diamond pattern using '*'. See examples for more details.
+
+Input Format
+
+First line of input contains T - number of test cases. Its followed by T lines, each line contains a single odd integer N - the size of the diamond.
+
+Constraints
+
+1 <= T <= 100
+3 <= N <= 201
+
+Output Format
+
+For each test case, print the test case number as shown, followed by the diamond pattern, separated by newline.
+
+Sample Input 0
+
+4
+3
+7
+5
+15
+Sample Output 0
+
+Case #1:
+ *
+* *
+ *
+Case #2:
+   *
+  * *
+ *   *
+*     *
+ *   *
+  * *
+   *
+Case #3:
+  *
+ * *
+*   *
+ * *
+  *
+Case #4:
+       *
+      * *
+     *   *
+    *     *
+   *       *
+  *         *
+ *           *
+*             *
+ *           *
+  *         *
+   *       *
+    *     *
+     *   *
+      * *
+       *
+Explanation 0
+
+Self Explanatory
+
+*/
+
 import java.io.*;
 import java.util.*;
 import java.text.*;
