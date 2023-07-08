f43e4df98174b3e29fd394561c0a888fa33e0911
HEMANTH KUMAR REDDY TATAGARI <110995141+HemuTatagari@users.noreply.github.com>
2023-07-08 23:21:10 +0530

    Update sum_of_array_elements.java

--- sum_of_array_elements.java
+++ sum_of_array_elements.java
@@ -1,3 +1,39 @@
+/*
+
+Given an array of integers, find the sum of the elements of the given array.
+Note: Try to solve this without declaring/storing the array elements.
+
+Input Format
+
+First line of input contains T - number of test cases. Its followed by 2T lines, the first line contains N - the size of the array and second line contains the elements of the array.
+
+Constraints
+
+1 <= T <= 100
+1 <= N <= 1000
+1 <= ar[i] <= 1015
+
+Output Format
+
+For each test case, print the sum of the elements of the array, separated by new line.
+
+Sample Input 0
+
+2
+3
+5 15 3 
+2
+70 100 
+Sample Output 0
+
+23
+170
+Explanation 0
+
+Self Explanatory
+
+*/
+
 
 import java.util.*;
 
